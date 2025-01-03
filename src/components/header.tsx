6ebe00bd0fc8bdfd410e3c2c556fc6e9ce977b98
MWANGIKENNEDY <kenmwan75@gmail.com>
2025-01-03 20:39:25 +0300

    fixed the bug

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Header = () => {
     return (
       <section className="dark:bg-gray-900">
@@ -15,7 +17,7 @@ const Header = () => {
             <div className="absolute inset-0">
               <header>
                 <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 ">
-                  <a className="block text-teal-600" href="#">
+                  <Link className="block text-teal-600" href="#">
                     <span className="sr-only">Home</span>
                     <svg
                       className="h-8"
@@ -28,25 +30,25 @@ const Header = () => {
                         fill="currentColor"
                       />
                     </svg>
-                  </a>
+                  </Link>
   
                   <div className="flex flex-1 items-center justify-end md:justify-between  ">
                     <nav aria-label="Global" className="hidden md:block">
                       <ul className="flex items-center gap-6 text-sm">
                         <li>
-                          <a className="text-white transition " href="#">
+                          <Link className="text-white transition " href="#">
                             About
-                          </a>
+                          </Link>
                         </li>
                         <li>
-                          <a className="text-white transition " href="#">
+                          <Link className="text-white transition " href="#">
                             Careers
-                          </a>
+                          </Link>
                         </li>
                         <li>
-                          <a className="text-white transition " href="#">
+                          <Link className="text-white transition " href="#">
                             Services
-                          </a>
+                          </Link>
                         </li>
                       </ul>
                     </nav>
